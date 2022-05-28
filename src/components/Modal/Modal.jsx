8616a7d8980e5519d0e54342ffaae635298c5495
Serhii Korchemkin <skorchemkin1987@gmail.com>
2022-05-28 17:16:08 +0300

    fix

---
+++ src/components/Modal/Modal.jsx
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import { createPortal } from 'react-dom';
+import s from './modal.module.css';
+
+const modalRoot = document.querySelector('#modal-root');
+
+class Modal extends Component {
+  componentDidMount() {
+    window.addEventListener('keydown', this.handleKeyDown);
+  }
+  handleKeyDown = e => {
+    if (e.code === 'Escape') {
+      console.log('Eskape');
+      this.props.onClose();
+    }
+  };
+  handleBackropClick=e=>{
+    if (e.currentTarget === e.target) {
+        console.log('Eskape');
+        this.props.onClose();
+      }
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('keydown', this.handleKeyDown);
+  }
+  render() {
+    return createPortal(
+      <div className={s.Overlay} onClick={this.handleBackropClick}>
+        <div className={s.Modal}>{this.props.children}</div>
+      </div>,
+      modalRoot
+    );
+  }
+}
+
+export default Modal;
